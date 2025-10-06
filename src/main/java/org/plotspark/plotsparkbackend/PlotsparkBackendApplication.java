c18740d57f2a3f834d4ab52f21b011a824296280
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2025-10-07 00:14:16 +0530

    Initial Project Setup

---
+++ src/main/java/org/plotspark/plotsparkbackend/PlotsparkBackendApplication.java
@@ -0,0 +1,13 @@
+package org.plotspark.plotsparkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotsparkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlotsparkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/plotspark/plotsparkbackend/PlotsparkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.plotspark.plotsparkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlotsparkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
